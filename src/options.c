6bcd3c76c1c773b3da57b616840d426a07737a60
Rod Guillaume <guilla_w@epita.fr>
2017-11-30 00:13:24 +0100

    options: [+-]O short options parsing written         Still have to test it, and write get_files parsing function

--- src/options.c
+++ src/options.c
@@ -74,32 +74,43 @@ static void options_c(size_t argc, char **argv,
                              struct options *opts, size_t *i)
 {
   // TODO
+  // Have to clarify stuff about positional parameters first
   argc = argc;
   argv = argv;
   opts = opts;
   i = i;
 }
 
-static void shopt_option_warn(const char *opt)
-{
-  // TODO
-  opt = opt;
-} 
-static int is_shopt_option(const char *opt)
-{
-  // TODO
-  shopt_option_warn(opt);
-  opt = opt;
-  return 1;
-}
-
-static void shopt_handle_option(const char *opt, int set,
+static int shopt_handle_option(const char *opt, int set,
                                 struct options *opts)
 {
-  // TODO
-  set = set;
-  opt = opt;
-  opts = opts;
+  char *shopt_options[] = { "ast_print", "dotglob", "expand_aliases",
+                            "extglob", "nocaseglob", "nullglob", "sourcepath",
+                            "xpg_echo", 0 };
+  if (!strcmp(shopt_options[0], opt))
+    opts->shopt_vars |= set ? AST_PRINT : 0;
+  else if (!strcmp(shopt_options[1], opt))
+    opts->shopt_vars |= set ? DOTGLOB : 0;
+  else if (!strcmp(shopt_options[2], opt))
+    opts->shopt_vars |= set ? EXPAND_ALIASES : 0;
+  else if (!strcmp(shopt_options[3], opt))
+    opts->shopt_vars |= set ? EXTGLOB : 0;
+  else if (!strcmp(shopt_options[4], opt))
+    opts->shopt_vars |= set ? NOCASEGLOB : 0;
+  else if (!strcmp(shopt_options[5], opt))
+    opts->shopt_vars |= set ? NULLGLOB : 0;
+  else if (!strcmp(shopt_options[6], opt))
+    opts->shopt_vars |= set ? SOURCEPATH : 0;
+  else if (!strcmp(shopt_options[7], opt))
+    opts->shopt_vars |= set ? XPG_ECHO : 0;
+  else
+  {
+    fprintf(stdout, "%s: invalid shopt option\n"
+            "shopt_options available: ast_print dotglob expand_aliases"
+            " extglob nocaseglob nullglob sourcepath xpg_echo", opt);
+    return 0;
+  }
+  return 1;
 }
 
 static int is_short_option(const char *opt)
@@ -114,16 +125,11 @@ static int is_short_option(const char *opt)
 static void options_shopt(size_t argc, char **argv,
                              struct options *opts, size_t *i)
 {
-  // TODO
-  argc = argc;
-  // -O sets the value, +O unsets it
-  // if value does not exist, print all available values
   int is_set = argv[(*i)++][0] == '-';
-  for (; !is_short_option(argv[*i]); (*i)++)
+  for (; *i < argc && !is_short_option(argv[*i]); (*i)++)
   {
-    if (!is_shopt_option(argv[*i]))
+    if (!shopt_handle_option(argv[*i], is_set, opts))
       break;
-    shopt_handle_option(argv[*i], is_set, opts);
   }
 }
 
@@ -142,10 +148,6 @@ static void options_short_get(size_t argc, char **argv,
                              struct options *opts, size_t *i)
 {
   // TODO
-  argc = argc;
-  argv = argv;
-  opts = opts;
-  i = i;
   int is_c_enabled = 0;
   for (; *i < argc && is_short_option(argv[*i]); (*i)++)
   {
@@ -158,18 +160,13 @@ static void options_short_get(size_t argc, char **argv,
     if (!opt[1] && opt[0] == 'c')
       is_c_enabled = 1;
     else if (!opt[1] && opt[0] == 'O')
-      // +-O option TODO
       options_shopt(argc, argv, opts, i);
     else
       errx(1, "%s: invalid option\n"
               "Usage: 42sh [GNU long options] [options] [file]", opt);
   }
   if (is_c_enabled)
-  {
-    // -c option string and files to handle, now that all options have been
-    // done
     options_c(argc, argv, opts, i);
-  }
 }
 
 /**
