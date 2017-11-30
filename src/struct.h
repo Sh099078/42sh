ea3a3144e22f0c33656612df965be1404523fcff
Loup Federico <federi_l@epita.fr>
2017-11-30 22:52:02 +0100

    struct.h: modify the list structs
      - str_lst is now aliases_lst; fct_lst is functions_lst;
        var_lst is now variables_lst.
      - aliases_lst now contains two strings: the pattern and its
        corresponding alias.

--- src/struct.h
+++ src/struct.h
@@ -1,20 +1,21 @@
 #ifndef STRUCT_H
 #define STRUCT_H
 
-struct str_lst
+struct aliases_lst
 {
-  char *string;
+  char *pattern; //pattern to be identified as an alias.
+  char *alias //string to substitute to the pattern.
   struct str_lst *next;
 };
 
-struct fct_lst
+struct functions_lst
 {
   char *name;
   struct *ast *ast;
   struct ast_lst *next;
 };
 
-struct var_lst
+struct variables_lst
 {
   char *name;
   char *value;
