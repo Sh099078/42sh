46c8cbe89e85e58dd4c8d420edb1859a5cdf7f8e
Rod Guillaume <guilla_w@epita.fr>
2017-12-05 20:03:07 +0100

    ast_exec: and_or rule written

---
+++ src/exec/and_or.c
@@ -0,0 +1,11 @@
+#include "ast_exec.h"
+
+int exec_and_or(struct ast *ast)
+{
+  if (ast->nb_children < 2)
+    return 1; // TODO ERROR SCL
+  int res = ast_exec(ast->children[0]);
+  if (res && strcmp(ast->value, "&&"))
+    return res;
+  return ast_exec(ast->children[1]);
+}
--- src/exec/ast_exec.h
+++ src/exec/ast_exec.h
@@ -13,6 +13,9 @@
 
 char **cmd_get_args(struct ast *ast, size_t start);
 char *cmd_get_path(char *cmd_name);
+int exec_simple_command(struct ast *ast)
+int exec_and_or(struct ast *ast);
+int exec_rule_if(struct ast *ast);
 int ast_exec(struct ast *ast);
 
 #endif /* AST_EXEC_H */
