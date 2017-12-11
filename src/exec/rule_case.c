43bc3d8e8a7c5ad9a86d5484b5774f6f63de142b
HamzaM <hamza.mebarek@epita.fr>
2017-12-11 22:40:07 +0100

    rule_case: implemented clause check

--- src/exec/rule_case.c
+++ src/exec/rule_case.c
@@ -1,1 +1,26 @@
-/Init/
+#include "ast_exec.h"
+
+int rule_case(struct ast *ast)
+{
+  char *var = ast->children[0]->value;
+  for(int i = 1 ; ast->children[i] ; i++)
+  {
+
+  }
+}
+
+int case_clause(struct ast *ast, char *var)
+{
+  int i = 0;
+  int ok = 0;
+  while(children[i]->type == 21 && ok == 0)
+  {
+    if(strcmp(children[i]->value, var) == 0)
+      ok++;
+    i++;
+  }
+  if(ok == 0)
+    return -1;
+  else
+    return ast_exec(ast->children[i]);
+}
