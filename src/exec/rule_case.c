6e95486ef996eceb777e1fe949bcfea499cb98e2
HamzaM <hamza.mebarek@epita.fr>
2017-12-13 17:56:51 +0100

    rule_case: fixed compilation errors

--- src/exec/rule_case.c
+++ src/exec/rule_case.c
@@ -1,23 +1,12 @@
-#include "ast_exec.h"
-
-int rule_case(struct ast *ast)
-{
-  char *var = ast->children[0]->value;
-  for(int i = 1 ; ast->children[i] ; i++)
-  {
-    if(case_clause(ast_children[i], var) != -1)
-      return 1;
-  }
-  return 0;
-}
+#include "../../includes/ast_exec.h"
 
 int case_clause(struct ast *ast, char *var)
 {
   int i = 0;
   int ok = 0;
-  while(children[i]->type == 21 && ok == 0)
+  while(ast->children[i]->type == 21 && ok == 0)
   {
-    if(strcmp(children[i]->value, var) == 0)
+    if(strcmp(ast->children[i]->value, var) == 0)
       ok++;
     i++;
   }
@@ -25,4 +14,15 @@ int case_clause(struct ast *ast, char *var)
     return -1;
   else
     return ast_exec(ast->children[i]);
+}
+
+int rule_case(struct ast *ast)
+{
+  char *var = ast->children[0]->value;
+  for(int i = 1 ; ast->children[i] ; i++)
+  {
+    if(case_clause(ast->children[i], var) != -1)
+      return 1;
+  }
+  return 0;
 }
