8c0053bafc15f875bea4fa67a0f36cc16d96f126
HamzaM <hamza.mebarek@epita.fr>
2017-12-15 00:31:46 +0100

    ast_printer: update according to new ast struct

--- src/print/ast_printer.c
+++ src/print/ast_printer.c
@@ -1,5 +1,5 @@
-#include "ast.h"
-#include "ast_printer.h"
+#include "../../includes/ast.h"
+#include "../../includes/ast_printer.h"
 
 
 char *get_type1(int x)
@@ -62,7 +62,7 @@ char *get_type2(int x)
   return NULL;
 }
 
-FILE *ast_children(struct ast *ast, FILE *file)
+/*FILE *ast_children(struct ast *ast, FILE *file)
 {
   char *type;
   char *value;
@@ -87,26 +87,97 @@ FILE *ast_children(struct ast *ast, FILE *file)
     file = ast_children(ast->children[i], file);
   }
   return file;
+}*/
+
+
+FILE *ast_children(struct ast *ast, FILE *file)
+{
+  char *data;
+  void *tmp_ast = ast;
+  void *tmp_chili;
+  if(ast->nb_children == 0)
+  {
+    for(int i = 0 ; ast->values[i] ; i++)
+    {
+      tmp_chili = &ast->values[i];
+      fprintf(file, "\"%p\" [label=\"%s\"]\n", tmp_chili, ast->values[i]);
+      fprintf(file, "\"%p\" -> \"%p\"\n", tmp_ast, tmp_chili);
+    }
+  }
+  else
+  {
+    if(ast->type <10)
+      data = get_type1(ast->type);
+    else if(ast->type < 22)
+      data = get_type2(ast->type);
+    fprintf(file, "\"%p\" [label=\"%s\"]\n", tmp_ast, data);
+    for(size_t i = 0 ; i < ast->nb_children ; i++)
+    {
+      tmp_chili = ast->children[i];
+      if(ast->children[i]->type < 10)
+        data = get_type1(ast->children[i]->type);
+      else if(ast->children[i]->type < 22)
+        data = get_type2(ast->children[i]->type);
+      fprintf(file, "\"%p\" [label=\"%s\"]\n", tmp_chili, data);
+      fprintf(file, "\"%p\" -> \"%p\"\n", tmp_ast, tmp_chili);
+      file = ast_children(ast->children[i], file);
+    }
+  }
+  return file;
 }
+
 
 void ast_to_dot(struct ast *ast)
 {
   FILE *file = fopen("ast.dot", "w+");
   fprintf(file, "digraph G {\n");
-  char *type;
-  char *value;
+  char *data;
   void *tmp_ast = ast;
   if(ast && ast->nb_children == 0)
   {
     if(ast->type <10)
-      type = get_type1(ast->type);
+      data = get_type1(ast->type);
     else if(ast->type < 22)
-      type = get_type2(ast->type);
-    value = ast->values[0];
-    fprintf(file, "\"%p\" [label=\"%s : %s\"]\n", tmp_ast, type, value);
+      data = get_type2(ast->type);
+    fprintf(file, "\"%p\" [label=\"%s\"]\n", tmp_ast, data);
   }
   else if(ast)
     file = ast_children(ast, file);
   fprintf(file, "}");
   fclose(file);
+}
+
+int main()
+{
+  struct ast *asta = malloc(sizeof(struct ast));
+  asta->type = INPUT;
+  asta->nb_children = 3;
+  asta->children = malloc(sizeof(struct ast *));
+
+  struct ast *astb = malloc(sizeof(struct ast));
+  astb->values = malloc(sizeof(char **));
+  astb->values[0] = "cat";
+  astb->values[1] = "a";
+  astb->type = COMMAND;
+  astb->nb_children = 0;
+
+  struct ast *astc = malloc(sizeof(struct ast));
+  astc->values = malloc(sizeof(char **));
+  astc->values[0] = "cat";
+  astc->values[1] = "b";
+  astc->type = SIMPLE_COMMAND;
+  astc->nb_children = 0;
+
+  struct ast *astd = malloc(sizeof(struct ast));
+  astd->values = malloc(sizeof(char **));
+  astd->values[0] = "echo";
+  astd->values[1] = "c";
+  astd->type = COMMAND;
+  astd->nb_children = 0;
+
+  asta->children[0] = astb;
+  asta->children[1] = astc;
+  asta->children[2] = astd;
+  ast_to_dot(asta);
+  return 1;
 }
