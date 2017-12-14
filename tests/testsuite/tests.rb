55d540a70c2ea3421a58fee7c1d6f1eff7544abd
Rod Guillaume <guilla_w@epita.fr>
2017-12-15 00:35:45 +0100

    test: Ruby script written. Not tested yet.

--- tests/testsuite/tests.rb
+++ tests/testsuite/tests.rb
@@ -2,54 +2,65 @@
 
 require 'rubygems'
 require 'bundler/setup'
-require 'inline'
 require 'open3'
+require 'json'
+require 'colorize'
 
-srcFiles = Dir.glob('../cmains/*.c')
-srcFiles.each do |src|
-  bin = File.basename(src, '*.c')
-  `gcc #{src} -o #{bin}`
-  inputFile = File.expand_path('.in', src)
-  File.readlines(inputFile).each do |line|
+=begin
+  *
+=end
+
+jsonFiles = Dir.glob('**/*.json')
+nbError = 0;
+jsonFiles.each do |file|
+  parsedJson = JSON.parse(file)
+  testName = parsedJson['name']
+  testArray = parsedJson['tests']
+  # Each JSON file contains an array of name-input hashmaps
+  testArray.each do |hash|
+    mybashOut = {}
     bashOut = {}
-    Open3.popen3(bin) do |stdin , stdout , stderr , t|
+    # Execute my 42sh renamed as bash
+    Open3.popen3('./bash') do |stdin , stdout , stderr , t|
       # write program input
-      stdin.puts line
+      stdin.puts hash['input']
       stdin.close
       # get output
-      42shOut[:stdout] = stdout.read
-      42shOut[:stderr] = stderr.read
-      42shOut[:exitcode] = t.value.exitstatus
+      mybashOut[:stdout] = stdout.read
+      mybashOut[:stderr] = stderr.read
+      mybashOut[:exitcode] = t.value.exitstatus
     end
-    42shOut = {}
-    Open3.popen3('/bin/sh') do |stdin , stdout , stderr , t|
+    # Execute real bash
+    Open3.popen3('./bin/bash') do |stdin , stdout , stderr , t|
       # write program input
-      stdin.puts line
+      stdin.puts hash['input']
       stdin.close
       # get output
       bashOut[:stdout] = stdout.read
       bashOut[:stderr] = stderr.read
       bashOut[:exitcode] = t.value.exitstatus
     end
-    # compare fileContent and 42shOut
-    $stdout.puts fileName + ' @' + bashOut[:name] + ':'
-    nbError = 0;
-    if 42shOut[:stdout] != bashOut[:stdout]
-      $stderr.puts '\tSTDOUT does not match'
+    # compare outputs from bash and 42sh
+    testNbError = nbError
+    $stdout.puts "\t #{hash['name']}"
+    if mybashOut[:stdout] != bashOut[:stdout]
+      $stderr.puts '\tSTDOUT does not match'.colorize(:red)
       nbError += 1;
     end
-    if 42shOut[:stdout] != bashOut[:stdout]
-      $stderr.puts '\tSTDERR does not match'
+    if mybashOut[:stderr] != bashOut[:stderr]
+      $stderr.puts '\tSTDERR does not match'.colorize(:red)
       nbError += 1;
     end
-    if 42shOut[:stdout] != bashOut[:stdout]
-      $stderr.puts '\tEXIT STATUS does not match'
+    if mybashOut[:exitcode] != bashOut[:exitcode]
+      $stderr.puts '\tEXIT STATUS does not match'.colorize(:red)
       nbError += 1;
     end
-    if nbError > 0
-      $stderr.puts bashOut[:name] + 'failed with ' + nbError.to_s + ' errors.'
+    if nbError != testNbError
+      $stderr.puts "#{hash['name']} failed with #{nbError - testNbError} errors.".colorize(:red)
     else
-      $stdout.puts bashOut[:name] + 'passed succesfully.'
+      $stdout.puts "#{bashOut['name']} passed succesfully."
     end
   end
+  if nbError > 0
+    $stdout.puts "${parsedJson['name']} "
 end
