3c38aa30acc44b3bd63e45ee09a1c5415902c5f6
Loup Federico <federi_l@epita.fr>
2017-12-15 23:31:17 +0100

    parser: fix compilation error

--- src/parser/parser_2.c
+++ src/parser/parser_2.c
@@ -24,6 +24,7 @@ struct ast *parse_element(int *return_value, struct context *context)
     ast_destroy(child);
     return NULL;
   }
+  return_value = return_value;
 
   context->token_used = 1;
   element->type = ELEMENT;
