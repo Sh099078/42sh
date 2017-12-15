c0a1f94c175ece1014f6a6b806c28f12d7707182
Rod Guillaume <guilla_w@epita.fr>
2017-12-15 01:19:05 +0100

    test: Ruby testsuite done. Fully working and colored

--- tests/tests.rb
+++ tests/tests.rb
@@ -6,16 +6,14 @@ require 'open3'
 require 'json'
 require 'colorize'
 
-=begin
-  *
-=end
-
-jsonFiles = Dir.glob('**/*.json')
+jsonFiles = Dir.glob('inputs/*.json')
 nbError = 0;
 jsonFiles.each do |file|
-  parsedJson = JSON.parse(file)
+  content = File.read(file)
+  parsedJson = JSON.parse(content)
   testName = parsedJson['name']
   testArray = parsedJson['tests']
+  $stdout.puts "\t\#\#\#\#\#   #{testName}   \#\#\#\#\#\n".colorize(:light_blue)
   # Each JSON file contains an array of name-input hashmaps
   testArray.each do |hash|
     mybashOut = {}
@@ -31,7 +29,7 @@ jsonFiles.each do |file|
       mybashOut[:exitcode] = t.value.exitstatus
     end
     # Execute real bash
-    Open3.popen3('./bin/bash') do |stdin , stdout , stderr , t|
+    Open3.popen3('/bin/bash') do |stdin , stdout , stderr , t|
       # write program input
       stdin.puts hash['input']
       stdin.close
@@ -42,25 +40,31 @@ jsonFiles.each do |file|
     end
     # compare outputs from bash and 42sh
     testNbError = nbError
-    $stdout.puts "\t #{hash['name']}"
-    if mybashOut[:stdout] != bashOut[:stdout]
-      $stderr.puts '\tSTDOUT does not match'.colorize(:red)
+    $stdout.puts "\n\t\#\#\#   #{hash['name']}   \#\#\#".colorize(:light_blue)
+    if mybashOut['stdout'] != bashOut['stdout']
+      $stderr.puts "STDOUT does not match".colorize(:light_red)
+      $stderr.puts "expected:\n#{bashOut['stdout']}\n42sh:\n#{mybashOut['stdout']}"
       nbError += 1;
     end
-    if mybashOut[:stderr] != bashOut[:stderr]
-      $stderr.puts '\tSTDERR does not match'.colorize(:red)
+    if mybashOut['stderr'] != bashOut['stderr']
+      $stderr.puts "STDERR does not match".colorize(:light_red)
+      $stderr.puts "expected:\n#{bashOut['stderr']}\n42sh:\n#{mybashOut['stderr']}"
       nbError += 1;
     end
     if mybashOut[:exitcode] != bashOut[:exitcode]
-      $stderr.puts '\tEXIT STATUS does not match'.colorize(:red)
+      $stderr.puts "EXIT STATUS does not match".colorize(:light_red)
+      $stderr.puts "expected:\n#{bashOut[:exitcode]}\n42sh:\n#{mybashOut['exitcode']}"
       nbError += 1;
     end
     if nbError != testNbError
-      $stderr.puts "#{hash['name']} failed with #{nbError - testNbError} errors.".colorize(:red)
+      $stderr.puts "\n#{hash['name']} failed with #{nbError - testNbError} errors.".colorize(:light_red)
     else
-      $stdout.puts "#{bashOut['name']} passed succesfully."
+      $stdout.puts "\n#{hash['name']} passed succesfully."
     end
   end
   if nbError > 0
-    $stdout.puts "${parsedJson['name']} "
+    $stderr.puts "\n#{parsedJson['name']} tests finished with #{nbError} errors".colorize(:red)
+  else
+    $stdout.puts "\n#{parsedJson['name']} tests finished succesfully".colorize(:light_green)
+  end
 end
