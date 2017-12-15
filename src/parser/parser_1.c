c126ac55ead43bbeb913a42a13adaaa5b80eae28
Loup Federico <federi_l@epita.fr>
2017-12-15 17:04:42 +0100

    parser: add abort_parsing function to fit coding style

--- src/parser/parser_1.c
+++ src/parser/parser_1.c
@@ -4,45 +4,46 @@
 #include "lexer.h"
 #include "string.h"
 
+static struct ast* abort_parsing(struct ast *ast, int *return_value)
+{
+  *return_value = 1; //error
+  ast_destroy(ast);
+  return NULL;
+}
+
 struct ast *parse_list(int *return_value, struct context *context)
 {
   struct ast *list = ast_init();
   struct ast *and_or = parse_and_or(return_value, context);
+
   if (!list)
-  {
-    *return_value = 1; //error
-    ast_destroy(and_or);
-    return NULL;
-  }
+    return abort_parsing(and_or, return_value);
+
   for (; and_or; and_or = parse_and_or(return_value, context))
   {
     get_next_token(context);
-    ast_add_child(list, and_or, context->token->token);
     char *token = context->token->token;
+    ast_add_child(list, and_or, token);
     if (strcmp(token, ";") && strcmp(token, "&"))
     {
       context->token_used = 0;
       break;
     }
   }
+
   if (!list->nb_children)
-  {
-    *return_value = 1; //error
-    ast_destroy(list);
-    return NULL;
-  }
+    return abort_parsing(list, return_value);
+
   return list;
 }
+
 struct ast *parse_and_or(int *return_value, struct context *context)
 {
   struct ast *and_or = ast_init();
   struct ast *pipeline = parse_pipeline(return_value, context);
   if (!and_or)
-  {
-    *return_value = 1; //error
-    ast_destroy(pipeline);
-    return NULL;
-  }
+    return abort_parsing(pipeline, return_value);
+
   for (; pipeline; pipeline = parse_pipeline(return_value, context))
   {
     get_next_token(context);
@@ -58,27 +59,23 @@ struct ast *parse_and_or(int *return_value, struct context *context)
       context->token_used = 1;
       get_next_token(context);
     }
+  }
 
-  }
   if (and_or->nb_children == 0)
-  {
-    ast_destroy(and_or);
-    *return_value = 1; //error
-    return NULL;
-  }
+    return abort_parsing(and_or, return_value);
+
   return and_or;
 }
 
 struct ast *parse_pipeline(int *return_value, struct context *context)
 {
   struct ast *pipeline = ast_init();
+
   if (!(get_next_token(context) && pipeline))
-  {
-    *return_value = 1; //error
-    ast_destroy(pipeline);
-    return NULL;
-  }
+    return abort_parsing(pipeline, return_value);
+
   context->token_used = context->token->type == Bang ? 1 : 0;
+
   struct ast *command = parse_command(return_value, context);
   for (; command; command = parse_command(return_value, context))
   {
@@ -93,5 +90,6 @@ struct ast *parse_pipeline(int *return_value, struct context *context)
       context->token_used = 1;
   }
 }
+
 struct ast *parse_command(int *return_value, struct context *context);
 struct ast *parse_simple_command(int *return_value, struct context *context);
