24f3f8dedc64e0ac38836a15682431d585db5e27
HamzaM <hamza.mebarek@epita.fr>
2017-12-15 22:46:17 +0100

    rule_case: added struct env according to struct update

--- src/exec/rule_case.c
+++ src/exec/rule_case.c
@@ -1,6 +1,6 @@
-#include "../../includes/ast_exec.h"
+#include "ast_exec.h"
 
-int case_clause(struct ast *ast, char *var)
+int case_clause(struct shell_env *env, struct ast *ast, char *var)
 {
   int i = 0;
   int ok = 0;
@@ -13,15 +13,15 @@ int case_clause(struct ast *ast, char *var)
   if(ok == 0)
     return -1;
   else
-    return ast_exec(ast->children[i]);
+    return ast_exec(env, ast->children[i]);
 }
 
-int rule_case(struct ast *ast)
+int rule_case(struct shell_env *env, struct ast *ast)
 {
   char *var = ast->children[0]->values[0];
   for(int i = 1 ; ast->children[i] ; i++)
   {
-    if(case_clause(ast->children[i], var) != -1)
+    if(case_clause(env, ast->children[i], var) != -1)
       return 1;
   }
   return 0;
