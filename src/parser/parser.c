e8d46b8b0a14837cdf65f20a0ae915cc646e123a
Loup Federico <federi_l@epita.fr>
2017-12-17 13:27:44 +0100

    memory_leaks: fix one in parser

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -18,6 +18,8 @@ struct ast *ast_create(int *return_value)
     &token, NULL, 0, 0, 1, 1, 0
   };
   struct ast *ast = parse_input(return_value, &context);
+  if (token.token)
+    free(token.token);
   return ast;
 }
 
