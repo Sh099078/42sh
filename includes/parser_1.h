f6225ce612a9ebcb7a0226b0163ce188d6ecbf77
Loup Federico <federi_l@epita.fr>
2017-12-17 19:44:09 +0100

    parser.h: add documentation

--- includes/parser_1.h
+++ includes/parser_1.h
@@ -3,10 +3,29 @@
 
 #include "ast.h"
 
+/**
+**  This function parses the 'list' rule of the grammar.
+**/
 struct ast *parse_list(int *return_value, struct context *context);
+
+/**
+** This function parses the 'and_or' rule of the grammar.
+*/
 struct ast *parse_and_or(int *return_value, struct context *context);
+
+/**
+**  This function parses the 'pipeline' rule of the grammar.
+**/
 struct ast *parse_pipeline(int *return_value, struct context *context);
+
+/**
+**  This function parses the 'command' rule of the grammar.
+**/
 struct ast *parse_command(int *return_value, struct context *context);
+
+/**
+**  This function parses the 'simple_command' rule of the grammar.
+**/
 struct ast *parse_simple_command(int *return_value, struct context *context);
 
 #endif /* PARSER_1_H */
